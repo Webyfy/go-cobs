69f4b3313d140e0e3be6795deec3cea9bab98a6a
dhan <dhanesh@webyfy.com>
2022-05-07 15:56:39 +0530

    Add encoding test

--- cobs_test.go
+++ cobs_test.go
@@ -6,7 +6,46 @@ import (
 	"testing"
 )
 
+type dataEncodedPair struct {
+	data    []byte
+	encoded []byte
+}
+
 func TestCobsEncoding(t *testing.T) {
+	dataEncodedPairs := []dataEncodedPair{
+		{
+			[]byte{0x00},
+			[]byte{0x01, 0x01, 0x00},
+		},
+		{
+			[]byte{0x00, 0x00},
+			[]byte{0x01, 0x01, 0x01, 0x00},
+		},
+		{
+			[]byte{0x00, 0x11, 0x00},
+			[]byte{01, 0x02, 0x11, 0x01, 0x00},
+		},
+		{
+			[]byte{0x11, 0x22, 0x00, 0x33},
+			[]byte{0x03, 0x11, 0x22, 0x02, 0x33, 0x00},
+		},
+		{
+			[]byte{0x11, 0x22, 0x33, 0x44},
+			[]byte{0x05, 0x11, 0x22, 0x33, 0x44, 0x00},
+		},
+		{
+			[]byte{0x11, 0x00, 0x00, 0x00},
+			[]byte{0x02, 0x11, 0x01, 0x01, 0x01, 0x00},
+		},
+	}
+
+	for _, item := range dataEncodedPairs {
+		encoded := Encode(item.data)
+		if !bytes.Equal(encoded, item.encoded) {
+			t.Errorf("Encoding test failed for input: %v", item.data)
+			t.Errorf("Expected: %v, got: %v", item.encoded, encoded)
+		}
+	}
 
 }
 
@@ -25,6 +64,6 @@ func TestCobsDecoding(t *testing.T) {
 	encodedBytes := Encode(data)
 	decodedBytes := Decode(encodedBytes)
 	if !bytes.Equal(data, decodedBytes) {
-		t.Errorf("Cobs Decoding test with trailing 0 failed")
+		t.Errorf("Cobs Decoding test failed")
 	}
 }
